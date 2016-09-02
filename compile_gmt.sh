e8d8ee70fafd0b15e9f43ec589d5de47c660d9a2
nuimura <nuimura@nagoya-u.jp>
2016-09-02 14:58:25 +0900

    comment in

--- compile_gmt.sh
+++ compile_gmt.sh
@@ -8,11 +8,11 @@ NCPU=4
 GSHHG_ROOT=/home/$USER/gshhg-gmt-${GSHHG_VERSION}
 
 
-# cd /home/$USER
-# wget ftp://ftp.soest.hawaii.edu/gmt/gmt-${GMT_VERSION}-src.tar.gz
-# wget ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-${GSHHG_VERSION}.tar.gz
-# tar xvzf gmt-${GMT_VERSION}-src.tar.gz
-# tar xvzf gshhg-gmt-${GSHHG_VERSION}.tar.gz
+cd /home/$USER
+wget ftp://ftp.soest.hawaii.edu/gmt/gmt-${GMT_VERSION}-src.tar.gz
+wget ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-${GSHHG_VERSION}.tar.gz
+tar xvzf gmt-${GMT_VERSION}-src.tar.gz
+tar xvzf gshhg-gmt-${GSHHG_VERSION}.tar.gz
 
 
 cd gmt-${GMT_VERSION}
@@ -23,10 +23,10 @@ sed -e 's/\#set (COPY_GSHHG TRUE)/set (COPY_GSHHG TRUE)/g' \
 > ./cmake/ConfigUser.cmake
 
 
-# cp /home/$USER/d/Mydocument/Dropbox/program/gmt/ConfigUser.cmake ./cmake/ConfigUser.cmake
-# mkdir build
-# cd build
-# cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-# ncpu=$(nproc)
-# make -j${ncpu}
-# sudo make -j${ncpu} install
+cp /home/$USER/d/Mydocument/Dropbox/program/gmt/ConfigUser.cmake ./cmake/ConfigUser.cmake
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+ncpu=$(nproc)
+make -j${ncpu}
+sudo make -j${ncpu} install
